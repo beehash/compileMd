22eaa1b522c04d09805a409bfdf8d5be99a405cd
amsiney@xingren.com <aminsey@xingren.com>
2020-11-24 12:11:34 +0800

    path change

---
+++ config.global.js
@@ -0,0 +1,10 @@
+import { link } from "fs";
+
+const path = location.path;
+const head = document.getElementsByTagName('head')[0];
+
+export function requireModule (cssList, jsList, cb) {
+  const promiseList = cssList.map((item) => {
+    
+  });
+}
